0612a333abcfdf6e6dfe8a4c7c867a744967d18e
Ksenia Gurenko <gurenkoksenia0@gmail.com>
2024-05-05 15:58:08 +0300

    props

--- react-practic/src/content-left/Welcome.jsx
+++ react-practic/src/content-left/Welcome.jsx
@@ -1,19 +1,23 @@
 import "../content-left/welcome.css";
+import { Title } from "./title/Title";
 
-const text =
-  "Here you will find the cutest and funniest kittens for your home. Immerse yourself in a world of purring furry friends and choose your perfect kitten right now. Welcome to our cat family!";
+const title = "Happiness store";
 
 export function Welcome() {
   return (
     <>
       <div className="post">
-        <h1 className="big_text">Happiness store</h1>
+        <Title title={title} />
         <div className="group">
           {[...Array(3)].map((item, index) => (
             <div className="circle" key={index} />
           ))}
         </div>
-        <p className="small_text">{text}</p>
+        <p className="small_text">
+          Here you will find the cutest and funniest kittens for your home.
+          Immerse yourself in a world of purring furry friends and choose your
+          perfect kitten right now. Welcome to our cat family!
+        </p>
       </div>
     </>
   );
---
+++ react-practic/src/content-left/title/Title.jsx
@@ -0,0 +1,7 @@
+export function Title({ title }) {
+  return (
+    <>
+      <h1 className="big_text">{title}</h1>
+    </>
+  );
+}
